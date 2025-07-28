49fddde271f23cc4f234bb2311b4abba91e035be
Waqar Ahmed <waqar.astrill@gmail.com>
2025-07-28 08:54:34 +0500

    updated icons and logo

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,41 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/nexio/sw.js');
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/nexio/sw.js");
+  });
 }
-if(!navigator.onLine) window.location.href = '/nexio/offline.html';
+if (!navigator.onLine) window.location.href = "/nexio/offline.html";
 const url = window.location.href;
-    function getPath() {
-        const path = window.location.pathname
-            .replace(/^\/nexio\//, '')
-            .replace(/\/$/, '');
-        return path || 'home';
-    }
 
-    // Update iframe with current path
-    function updateIframe() {
-        let path = getPath();
-        document.title = 'Nexio App - ' + cap(path);
-        const iframe = document.querySelector('iframe');
-        if (url.includes('?ssid=')) {
-            path = url.split('nexio/')[1].split('?').join('&');
-            console.log(url);
-        }
-        iframe.src = `https://script.google.com/macros/s/AKfycbxzTgJEEuZ0TPV-V7wYPbDKYMuKufa_le-DgMboGWKluB-iW1hIS5a4X89Rdl2tMwQA/exec?p=${path}`;
-        console.log(path);
-        iframe.addEventListener('load', function() {
-            document.querySelector('#load').style.display = 'none';
-            iframe.style.display = 'block';
-        });
-    }
-    function cap(str) {
-      if (!str) return ""; // Handle empty string
-      return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-    }
+function getPath() {
+  const path = window.location.pathname
+    .replace(/^\/nexio\//, "")
+    .replace(/\/$/, "");
+  return path || "home";
+}
 
-    // Initialize
-    document.addEventListener('DOMContentLoaded', function() {
-        document.querySelector('#qoute').innerHTML = "Next-Gen Electronics - Today";
-        updateIframe();
-        window.addEventListener('popstate', updateIframe);
-    });
+// Update iframe with current path
+function updateIframe() {
+  let path = getPath();
+  document.title = "Nexio App - " + cap(path);
+  const iframe = document.querySelector("iframe");
+  if (url.includes("?ssid=")) {
+    path = url.split("nexio/")[1].split("?").join("&");
+    console.log(url);
+  }
+  iframe.src = `https://script.google.com/macros/s/AKfycbxzTgJEEuZ0TPV-V7wYPbDKYMuKufa_le-DgMboGWKluB-iW1hIS5a4X89Rdl2tMwQA/exec?p=${path}`;
+  console.log(path);
+  iframe.addEventListener("load", function () {
+    document.querySelector("#load").style.display = "none";
+    iframe.style.display = "block";
+  });
+}
+function cap(str) {
+  if (!str) return ""; // Handle empty string
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+// Initialize
+document.addEventListener("DOMContentLoaded", function () {
+  updateIframe();
+  window.addEventListener("popstate", updateIframe);
+});
